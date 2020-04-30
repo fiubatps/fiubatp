6c18ff4f9e4e4344490de04d38200f22df4458f6
Adeodato Simó <dato@fi.uba.ar>
2020-04-30 16:08:34 -0300

    Separar TEAM_ID en DOC_ID (permiso maintain) y ADM_ID (admin).

--- mkrepo.sh
+++ mkrepo.sh
@@ -4,9 +4,14 @@
 # crear el repo sin ninguna configuración, y marcar un equipo como
 # admin.
 #
-# Se necesita solamente rellenar TEAM_SLUG: el nombre del equipo docente
-# (que debe existir en la organización); y TEAM_ID, el identificador
-# numérico correspondiente a ese equipo.
+# Se necesita solamente rellenar DOC_SLUG: el nombre del equipo docente
+# (que debe existir en la organización); y DOC_ID, el identificador
+# numérico correspondiente a ese equipo. A este equipo se le asigna
+# permiso de administración de los repositorios.
+#
+# Opcionalmente, se puede definir ADM_ID con otro identificador numérico
+# de equipo. De estar definido, se asigna permiso de administración a
+# este equipo, y a DOC_ID se le asigna permiso "maintain".
 #
 # La lista de repositorios a crear se obtiene de entrada estándar (en
 # formato "username repo_name"). Por ejemplo:
@@ -27,7 +32,21 @@ API="https://api.github.com"
 
 # El identificador numérico de un equipo "eqx" se puede obtener con:
 #   http -a ... $API/orgs/$ORG/teams | jq '.[] | select(.slug == "eqx") | .id'
-TEAM_ID=3141585
+
+### Sistemas operativos ###
+ADM_ID=3581967  # sisop-adm
+DOC_ID=3581970  # sisop-20a
+DOC_SLUG="sisop-20a"
+
+### Organización del Computador ###
+# ADM_ID=3581979  # orga-adm
+# DOC_ID=3581981  # orga-20a
+# DOC_SLUG="orga-20a"
+
+### Algoritmos y Programación II ###
+# ADM_ID=3581963  # algorw-adm
+# DOC_ID=3581965  # algorw-20a
+# DOC_SLUG="algorw-20a"
 
 # Autenticación (con API token) para el bot de la administración.
 USER="fiubatp"
@@ -59,10 +78,13 @@ while read users repo; do
     # Crear el repositorio.
     post "orgs/$ORG/repos" name:="\"$repo\"" private:=true
 
-    # Dar permisos al equipo docente. (En el futuro, si hay múltiples
-    # correctores, otorgar a este equipo acceso "push" en lugar de
-    # "admin", y crear un sub-equipo separado para administradores.)
-    put "teams/$TEAM_ID/repos/$ORG/$repo" permission:='"admin"'
+    # Dar permisos a los equipos docentes.
+    if [[ -v ADM_ID ]]; then
+        put "teams/$ADM_ID/repos/$ORG/$repo" permission:='"admin"'
+        put "teams/$DOC_ID/repos/$ORG/$repo" permission:='"maintain"'
+    else
+        put "teams/$DOC_ID/repos/$ORG/$repo" permission:='"admin"'
+    fi
 
     # Enviar la invitación.
     for user in $(echo $users | tr , ' '); do
